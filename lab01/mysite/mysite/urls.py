16a3443006db4dbc39716cb321d4290c506a9504
huyenho712002 <hohuyen712002@gmail.com>
2024-09-28 18:27:01 +0700

    tutorial 2

--- lab01/mysite/mysite/urls.py
+++ lab01/mysite/mysite/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Welcome to the homepage!")  # Tạo một response đơn giản cho trang chủ
 
 urlpatterns = [
-    path("polls/", include("polls.urls")),
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('polls/', include('polls.urls')),  # Đường dẫn polls đã được định nghĩa
+    path('', home),  # Đường dẫn cho trang chủ
 ]
